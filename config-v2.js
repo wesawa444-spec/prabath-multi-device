c6b4930ee67c76d260797e13eae9806d2af2ab53
wesawa444-spec <wesawa444@gmail.com>
2026-04-14 08:23:37 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
   const DATABASE = "mongodb://mongo:zdIEJlcrndfvqyyUqyJxVVBsLuEYYhvy@junction.proxy.rlwy.net:11496" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
 const BOT_NUMBER = "94743678375" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "PRABATH-MD~apikJWwK4CIuhij" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const SESSION_ID = "PRABATH-MD~qb1ZsV6OKim9yZ4" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
